8b9283973e38ea321d12c83c15f5a875c293caf6
amonn phillip <amonnphillip@gmail.com>
2017-09-13 09:07:14 -0700

    updated build script

--- makeaci.sh
+++ makeaci.sh
@@ -2,6 +2,6 @@ chmod a+x acbuild
 ./acbuild --debug begin
 ./acbuild --debug set-name amonncoreos
 ./acbuild --debug label add version "0.0.1"
-./acbuild --debug label add arch "arm64"
+./acbuild --debug label add arch "amd64"
 ./acbuild --debug write --overwrite "amonncoreos.aci"
 ./scbuild --debug end
